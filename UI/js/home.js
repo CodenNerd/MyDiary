355e98b05bdc3b5c0460de98b7e5ab42465b9bf7
AbdulAzeez <aatanda.dammy@gmail.com>
2018-07-24 14:31:07 +0100

    Tidying up the work

---
+++ UI/js/home.js
@@ -0,0 +1,22 @@
+//collapsible js code from www.alligator.io
+let myLabels = document.querySelectorAll('.lbl-toggle');
+
+		Array.from(myLabels).forEach(label => {
+			label.addEventListener('keydown', e => {
+    // 32 === spacebar
+    // 13 === enter
+    if (e.which === 32 || e.which === 13) {
+    	e.preventDefault();
+    	label.click();
+    };
+});
+		});
+
+
+let displayTooltip = () => document.getElementsByClassName('tooltip')[0].style.visibility = "visible";
+			
+			let unshowTooltip = () => document.getElementsByClassName('tooltip')[0].style.visibility = "hidden";
+
+			let showModal = () => document.getElementById('modal').style.display = "inline-block";
+
+			let hideModal = () => document.getElementById('modal').style.display = "none";
