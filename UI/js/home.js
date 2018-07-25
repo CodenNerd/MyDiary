99a1dc6261837554ea25a67afec1fe74a8b89e06
AbdulAzeez <aatanda.dammy@gmail.com>
2018-07-25 11:03:18 +0100

    More commit

--- UI/js/home.js
+++ UI/js/home.js
@@ -21,3 +21,16 @@ let myLabels = document.querySelectorAll('.lbl-toggle');
 
 			let hideModal = () => document.getElementById('modal').style.display = "none";
 
+			let modalContent = () =>{
+				const modalForm = document.forms['modalForm'];
+
+				let dateValue = modalForm['entryDate'].value = "MM/DD/YYYY";
+				let summaryValue = modalForm['entrySummary'].value = "...Summary";
+				let detailValue = modalForm['entryDetail'].value = "You are not implementing the core functionality yet, you are only building the UserInterface elements, pages and views!	● You are to create a pull request to elicit review and feedback for the UI template when you are done working on them ● Do not use any css frameworks e.g bootstrap, materialize .● Do not download or use an already built website template.";
+
+
+			}
+
+			let removeEntry = id =>{
+					document.getElementById(id).parentNode.innerHTML = null;
+			}	
